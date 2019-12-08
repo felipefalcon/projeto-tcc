0f2dc368259bf3cc782d3fcad5d1914f1e6a08db
Felipe Carneiro <felipefalconwolf@hotmail.com>
2019-12-08 13:44:31 -0200

    Redesign login e melhoras em códigos login

--- public/js/commom-functions.js
+++ public/js/commom-functions.js
@@ -1,13 +1,16 @@
 
 	// Variável do host onde estão os serviços node (Mudando aqui, muda de todos os arquivos)
-	nodeHost = "./";
-	
-	function loading(status){
-		if(status == 'hide'){
+	const nodeHost = "./";
+
+	// Function de Tela de Loading
+	// Chamar: loading();            => Chama a Tela de Loading
+	// Chamar: loading('hide')       => Desaparece com a Tela de Loading que foi chamada em algum local
+	function loading(status) {
+		if (status == 'hide') {
 			$.LoadingOverlay("hide");
-		}else{
+		} else {
 			$.LoadingOverlay("show", {
-				background: "rgba(63, 51, 74, 0.8)",
+				background: "rgba(59, 29, 78, 0.8)",
 				imageColor: "rgba(193, 55, 120, 0.82)",
 			});
 		}
@@ -15,63 +18,58 @@
 
 	// Variável responsável por cachear algumas informações dos usuários.
 	userInfo = JSON.parse(window.localStorage.getItem('userInfo'));
+
 	// Variável responsável por cachear todos os usuários
 	allUsersInfo = JSON.parse(window.localStorage.getItem('allUsersInfo'));
+
 	// Variável responsável por guardar o usuário que você enviará a msg
 	toUser = JSON.parse(window.localStorage.getItem('toUser'));
+
 	// Variável responsável por cachear todos os eventos
 	allEvents = JSON.parse(window.localStorage.getItem('allEvents'));
-	//console.log(userInfo);
-	//console.log(allEvents);
 
-
-	if((window.location.pathname == "index.html" || window.location.pathname == "/") && userInfo.email != undefined){
-		window.location.replace("main-view.html");
-	}
-	function setUserCache(user){
+	// Getters e Setters das Variáveis de Cache declaradas acima ^
+	function setUserCache(user) {
 		window.localStorage.setItem('userInfo', JSON.stringify(user));
 		userInfo = JSON.parse(window.localStorage.getItem('userInfo'));
-		//console.log(userInfo);
 	}
-	
-	function resetUserCache(){
+
+	function resetUserCache() {
 		window.localStorage.setItem('userInfo', '{}');
 		userInfo = JSON.parse(window.localStorage.getItem('userInfo'));
-		//console.log(userInfo);
 	}
 
-	function setAllUsersCache(users){
+	function setAllUsersCache(users) {
 		window.localStorage.setItem('allUsersInfo', JSON.stringify(users));
 		allUsersInfo = JSON.parse(window.localStorage.getItem('allUsersInfo'));
-		//console.log(userInfo);
-	}
-	
-	function resetAllUsersCache(){
-		window.localStorage.setItem('allUsersInfo', '{}');
-		allUsersInfo = JSON.parse(window.localStorage.getItem('AllUsersInfo'));
-		//console.log(userInfo);
 	}
 
-	function setToUser(user){
+	function resetAllUsersCache() {
+		window.localStorage.setItem('allUsersInfo', '{}');
+		allUsersInfo = JSON.parse(window.localStorage.getItem('AllUsersInfo'));
+	}
+
+	function setToUser(user) {
 		window.localStorage.setItem('toUser', user);
 		toUser = JSON.parse(window.localStorage.getItem('toUser'));
 	}
-	
-	function resetToUser(){
+
+	function resetToUser() {
 		window.localStorage.setItem('toUser', '{}');
 		toUser = JSON.parse(window.localStorage.getItem('toUser'));
 	}
 
-	function setAllEvents(events){
+	function setAllEvents(events) {
 		window.localStorage.setItem('allEvents', JSON.stringify(events));
 		allEvents = JSON.parse(window.localStorage.getItem('allEvents'));
-		//console.log(allEvents);
 	}
-	
-	function resetAllEvents(){
+
+	function resetAllEvents() {
 		window.localStorage.setItem('allEvents', '{}');
 		allEvents = JSON.parse(window.localStorage.getItem('allEvents'));
-		//console.log(userInfo);
 	}
-	
-	
+
+	//Auto-login, verifica se há cache do Usuário se houve automaticamente Muda para Tela Principal
+	if ((window.location.pathname == "index.html" || window.location.pathname == "/") && userInfo.email != undefined) {
+		window.location.replace("main-view.html");
+	}
--- public/js/login.js
+++ public/js/login.js
@@ -1,67 +1,73 @@
 
-	var h = $(window).height()/10;
-	$("body").innerHeight($(window).height());
-	$("#logo-div").innerHeight(h*4.2);
-	$("#login-div").innerHeight(h*5.8);
+	(function () {
+		// Configurações iniciais da Tela de Login - Tamanho das DIVs
+		let h = $(window).height() / 10;
+		$("body").innerHeight($(window).height());
+		$("#login-div").innerHeight(h * 5.8);
+		$("#socials-div").css("height", $("#login-form-sub").css("height"));
 
-	var optionClicked = "btn-social";
-	var fadeDelay = 300;
+		// Variáveis de reuso e flag
+		let optionClicked = "btn-lero";
+		let fadeDelay = 300;
 
-	function loginUser(){
-		loading();
-		$.post(nodeHost+"con-user", { email: $("#email-input").val(), password: $("#password-input").val() })
-		.done(function( data ) {
-			if(data == null || data == "undefined"){
-				alert("Usuário não encontrado");
-			}else{
-				setUserCache(data);
-				window.location.replace("./main-view.html");
-			}
-			loading('hide');
+		// Clique do botão "REDE SOCIAL"
+		$("#btn-social").click(function () {
+			if (optionClicked != "btn-lero") return;
+			optionClicked = "btn-social";
+			$("#btn-social").fadeTo(fadeDelay, 1);
+			$("#btn-lero").fadeTo(fadeDelay, 0.3);
+			$("#socials-div").css("display", "block");
+			$("#socials-div").fadeTo(fadeDelay, 1);
+			$("#login-form-sub").fadeTo(fadeDelay, 0);
+			$("#login-form-sub").css("display", "none");
 		});
+
+		// Clique do botão "CONTA LeRo"
+		$("#btn-lero").click(function () {
+			if (optionClicked != "btn-social") return;
+			optionClicked = "btn-lero";
+			$("#btn-lero").fadeTo(fadeDelay, 1);
+			$("#btn-social").fadeTo(fadeDelay, 0.3);
+			$("#login-form-sub").css("display", "block");
+			$("#login-form-sub").fadeTo(fadeDelay, 1);
+			$("#socials-div").fadeTo(fadeDelay, 0);
+			$("#socials-div").css("display", "none");
+		});
+
+		// Quando o input do email está sem foco e tem texto, a fonte é aumentada
+		$("#email-input").focusout(function () {
+			if ($("#email-input").val().length > 0) {
+				$("#email-input").css("font-size", "16px");
+			}
+		});
+
+		// Quando o input do senha está sem foco e tem texto, a fonte é aumentada
+		$("#password-input").focusout(function () {
+			if ($("#password-input").val().length > 0) {
+				$("#password-input").css("font-size", "16px");
+			}
+		});
+
+	})();
+
+	// Function para logar o usuário
+	function loginUser() {
+		loading();
+		$.post(nodeHost + "con-user", { email: $("#email-input").val(), password: $("#password-input").val() })
+			.done(function (data) {
+				if (data == null || data === "undefined") {
+					alert("Usuário não encontrado");
+					loading('hide');
+				} else {
+					setUserCache(data);
+					window.location.replace("./main-view.html");
+				}
+			}).fail(function () {
+				alert("Houve uma falha");
+				loading('hide');
+			});
 		event.preventDefault();
 	}
 
-	$("#socials-div").css( "height" , $("#login-form-sub").css("height"));
-
-	$("#btn-social").click(function(){
-		if(optionClicked != "btn-lero"){
-			return;
-		}
-		optionClicked = "btn-social";
-		$("#btn-social").fadeTo( fadeDelay , 1);
-		$("#btn-lero").fadeTo( fadeDelay , 0.3);
-		$("#socials-div").css( "display" , "block");
-		$("#socials-div").fadeTo( fadeDelay , 1);
-		$("#login-form-sub").fadeTo( fadeDelay , 0);
-		$("#login-form-sub").css( "display" , "none");
-	});
-
-	$("#btn-lero").click(function(){
-		if(optionClicked != "btn-social"){
-			return;
-		}
-		optionClicked = "btn-lero";
-		$("#btn-lero").fadeTo( fadeDelay , 1);
-		$("#btn-social").fadeTo( fadeDelay , 0.3);
-		$("#login-form-sub").css( "display" , "block");
-		$("#login-form-sub").fadeTo( fadeDelay , 1);
-		$("#socials-div").fadeTo( fadeDelay , 0);
-		$("#socials-div").css( "display" , "none");
-	});
-
-	$("#email-input").focusout(function(){
-		if($("#email-input").val().length > 0){
-			$("#email-input").css("font-size", "16px");
-		}
-	});
-
-	$("#password-input").focusout(function(){
-		if($("#password-input").val().length > 0){
-			$("#password-input").css("font-size", "16px");
-		}
-	});
-
-	$("#btn-lero").click();
-	
+	// Chamada da Function que reseta o cache da conta do usuário (commom-functions.js)
 	resetUserCache();
