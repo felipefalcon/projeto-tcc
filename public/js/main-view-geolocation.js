c077e122044fb8178b35ac205ef555324206dc96
Felipe Carneiro <felipefalconwolf@hotmail.com>
2019-12-08 13:52:57 -0200

    Testes geolocaltion, erros e talz

--- public/js/main-view-geolocation.js
+++ public/js/main-view-geolocation.js
@@ -1,23 +1,25 @@
 	$("document").ready(function () {
 
-		navigator.geolocation.getCurrentPosition(function (posicao) {
+		navigator.geolocation.getCurrentPosition(sucessGeoLocation(posicao), failedGeoLocation(error));
+
+		function sucessGeoLocation(posicao) {
 			$.get("https://nominatim.openstreetmap.org/reverse?lat=" + posicao.coords.latitude + "&lon=" + posicao.coords.longitude + "&format=json").done(function (data) {
 				var location = {};
 				location = data.address;
 				location.region = data.display_name.split(",")[8];
 				$.get("./upd-user-location", {
-					email: 	userInfo.email,
+					email: userInfo.email,
 					location: location
-				}).done(function( data ) {
-					if(data == null || data == "undefined"){
+				}).done(function (data) {
+					if (data == null || data == "undefined") {
 						console.log("Deu merda");
-					}else{
+					} else {
 						$.get("./get-user", {
 							email: userInfo.email
-						}).done(function( data ) {
-							if(data == null || data == "undefined"){
+						}).done(function (data) {
+							if (data == null || data == "undefined") {
 								console.log("Deu merda");
-							}else{
+							} else {
 								setUserCache(data);
 								getProfile();
 							}
@@ -26,6 +28,14 @@
 					loading('hide');
 				});
 			});
-		});
+
+		}
+
+		function failedGeoLocation(error) {
+			if (error.code == error.PERMISSION_DENIED) {
+				// pop up dialog asking for location
+				alert("Você não deu permissão de localização");
+			}
+		}
 
 	});
