93ca9d4c1b54266e05e29d35933dd6c67e10a472
Felipe Carneiro <felipefalconwolf@hotmail.com>
2020-03-26 22:21:23 -0300

    Arrumado erro bobo em formto de data

--- public/js/create-profile-n.js
+++ public/js/create-profile-n.js
@@ -33,8 +33,8 @@
 		if("dt_nasc" in userInfo){
 			let dtNassFormat = new Date(userInfo.dt_nasc);
 			let monthFormat = dtNassFormat.getMonth()+1 < 10 ? "0" : "";
-			let dayFormat = dtNassFormat.getDate()+1 < 10 ? "0" : "";
-			$("#edit-age-input").val(dtNassFormat.getFullYear()+"-"+monthFormat+(dtNassFormat.getMonth()+1)+"-"+dtNassFormat.getDate());
+			let dayFormat = dtNassFormat.getDate() < 10 ? "0" : "";
+			$("#edit-age-input").val(dtNassFormat.getFullYear()+"-"+monthFormat+(dtNassFormat.getMonth()+1)+"-"+dayFormat+dtNassFormat.getDate());
 		}
 		addAnotherInfos();
 	}
