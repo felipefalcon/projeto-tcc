7d91ed54f08c572bef4481a4ec7baa2799f5fe88
Felipe Carneiro <felipefalconwolf@hotmail.com>
2020-03-28 00:39:00 -0300

    Novos blocos para usuários próximos

--- public/js/main-view.js
+++ public/js/main-view.js
@@ -181,7 +181,7 @@
 		allUsersInfo.forEach( function (data) {
 			var cityD = data.location ? data.location.city_district : "???";
 			createdDivs += "<div class='user-n-u-block'><div class='user-n-u-div'>"
-			+ "<label class='user-n-u-label'>" + data.name +" "+ data.lastname + "</label>"
+			+ "<label class='user-n-u-label'>" + data.name +" "+ data.lastname + "</label><i class='fas fa-user-circle view-prof-icon' ></i>"
 			+ "<div id='user-n-u-div-content' name='" + data._id + "' style='background-image: url(" + data.pics_url.main_pic + "'>"
 			+ "<div class='send-msg-button' name='" + data._id + "'><i class='fas fa-comment send-msg-icon' ></i></div></div>"
 			+ "<label id='city-district-n-u-label'>" + cityD + "</label></div></div>";
